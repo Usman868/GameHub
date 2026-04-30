d3a80254c8adb16324fb8e02d7b62168a8e093a5
Usman868 <usman03228270254@gmail.com>
2026-04-30 14:16:55 +0500

    added firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,18 +5,20 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId:     import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDkBsTHt44pjUNCXYkuz4g6YmMURVkW2nc",
+  authDomain: "gameshub-e10e9.firebaseapp.com",
+  projectId: "gameshub-e10e9",
+  storageBucket: "gameshub-e10e9.firebasestorage.app",
+  messagingSenderId: "578161280155",
+  appId: "1:578161280155:web:8fc3c2234f33add1fbf23e",
+  measurementId: "G-P44839LQ6L"
 };
 
-const app      = initializeApp(firebaseConfig);
-export const auth      = getAuth(app);
-export const db        = getFirestore(app);
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 export const analytics = getAnalytics(app);
 
 export default app;
